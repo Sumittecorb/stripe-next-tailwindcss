9e07577c10e657a08129c2bae2d0612d7c7d289d
Sumit Saxena <sumit.2019@tecorb.co>
2023-07-27 18:52:48 +0530

    Stripe setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import CardSection from "@/components/CardSelection/page"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
+      <CardSection />
+    </main>
+  )
+}
